2d2f1da60ea58ac6c500c9297e8e668c7c95f3e8
MathDev00 <116850455+MathDev00@users.noreply.github.com>
2023-06-17 15:22:37 -0300

    Initial commit

---
+++ src/testes/Testes.java
@@ -0,0 +1,18 @@
+package testes;
+
+import banco.Conexao;
+
+public class Testes {
+	
+	public static void main(String[] args) {
+		
+		
+		Conexao conexaoBd = new Conexao();
+		
+		conexaoBd.getConexao();
+
+	} 
+	
+	
+
+}
